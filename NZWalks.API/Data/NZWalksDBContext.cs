216c94ecea306c153217047d142ceedd9fa99112
anantsingh <anant.singh@mismosystems.com>
2024-08-16 06:41:48 +0530

    Add DI and run EF Migration.

--- NZWalks.API/Data/NZWalksDBContext.cs
+++ NZWalks.API/Data/NZWalksDBContext.cs
@@ -1,6 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Data
 {
-    public class NZWalksDBContext : DbContext { }
+    /*DbContext is a fundamental class in Entity Framework (EF), a popular Object-Relational Mapper (ORM) for .NET.
+    It represents a session with the database, allowing you to query and save data.
+    It is a bridge between your domain or entity classes and the database.*/
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions) { }
+
+        //Creating collection so that it can create tables in database...
+        //But we need to make a connection string as well..
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
 }
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+/*
+Dependency Injection (DI) is a design pattern used in software development to manage and inject dependencies into classes.
+It’s a way to achieve Inversion of Control (IoC) between classes and their dependencies, meaning that instead of the class creating its dependencies,
+they are passed to the class from the outside.
+*/
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Here used a dependency injection.....
+builder.Services.AddDbContext<NZWalksDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString"))
+);
+
+//then EF Migration.
 
 // Add controllers
 builder.Services.AddControllers();
